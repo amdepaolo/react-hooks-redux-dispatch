efeea18309163397e9243b8ef13a8532f8476c79
Adam DePaolo <amdepaolo@gmail.com>
2023-04-06 18:15:39 -0400

    commit tree b00e11bbe185f33fbd97361131505bc0167d46ae

--- src/reducer.js
+++ src/reducer.js
@@ -11,3 +11,16 @@ let state = { count: 0 };
 let action = { type: "counter/increment" };
 
 changeState(state, action);
+
+function render() {
+  const app = document.querySelector("#app");
+  app.textContent = state.count;
+}
+
+function dispatch(action) {
+  state = changeState(state, action);
+  render();
+}
+
+dispatch({type: "counter/increment"});
+dispatch({type: "counter/increment"});
